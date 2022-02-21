efb90bb4facd1e9bfed3a07ce33eff4766e4ef77
matiasmaizares <matias.maizares@mangoodev.com>
2022-02-21 15:13:11 -0300

    end of section 4

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -10,8 +10,6 @@ namespace Core.Interfaces
     {
         Task<Product> GetProductByIdAsync(int id);
         Task<IReadOnlyList<Product>> GetProductsAsync();
-
-
         Task<IReadOnlyList<ProductBrand>> GetProductBrandAsync();
         Task<IReadOnlyList<ProductType>> GetProductTypeAsync();
     }
